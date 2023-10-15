e34f89f9f248a4e3739a72a6fdaf689ff337180c
Jerry Nguyen Dinh <jdinh14@Administrators-MacBook-Pro.local>
2023-10-15 16:30:53 -0400

    folders and organized

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function subscribe() {
+    var email = document.getElementById('email').value;
+    if (validateEmail(email)) {
+        alert('Thank you for subscribing with the email: ' + email);
+        document.getElementById('message').textContent = 'Subscribed successfully!';
+    } else {
+        alert('Please enter a valid email address.');
+        document.getElementById('message').textContent = 'Failed to subscribe. Invalid email.';
+    }
+}
 
+function validateEmail(email) {
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    return email.match(pattern);
+}
